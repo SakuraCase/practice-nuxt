a49ae6560cbc9ffecb18a190b9388411f5b93f75
SakuraCase <sakurada6617@gmail.com>
2018-06-14 22:41:39 +0900

    tutorial1

--- pages/bulma/sample1.vue
+++ pages/bulma/sample1.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <a href="#" class="button is-primary is-large">hello</a>
+    <div class="columns">
+      <div class="column is-8">
+        <a href="#" class="button is-primary is-large">左</a>
+      </div>
+      <div class="column is-2">
+        <a href="#" class="button is-info is-medium">中</a>
+      </div>
+      <div class="column is-2">
+        <p>右</p>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-half">
+        xxxx
+      </div>
+      <div class="column is-half">
+        xxxx
+      </div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-one-quarter">
+        xxxx
+      </div>
+      <div class="column is-one-quarter">
+        xxxx
+      </div>
+      <div class="column is-one-quarter">
+        xxxx
+      </div>
+    </div>
+
   </div>
 </template>
 
