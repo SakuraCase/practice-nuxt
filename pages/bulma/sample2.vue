5ea5cf92b69b71bc67b0b574d85f6018b0b653a1
SakuraCase <sakurada6617@gmail.com>
2018-06-16 23:17:26 +0900

    css tutorial1-3

--- pages/bulma/sample2.vue
+++ pages/bulma/sample2.vue
@@ -48,18 +48,65 @@
           </div>
         </div>
       </section>
-
     </header>
 
     <main class="columns">
       <div class="submenu column is-3">
-        <aside>hoge</aside>
+        <aside class="box">
+          ほげほげ
+        </aside>
+        <aside class="box menu">
+          <p class="menu-label">おすすめのひとこと</p>
+          <ul class="menu-list">
+            <li><a href="#">HogeA<small>@a</small></a></li>
+            <li><a href="#">HogeB<small>@b</small></a></li>
+            <li><a href="#">HogeC<small>@c</small></a></li>
+          </ul>
+          <p class="menu-label">キーワード</p>
+          <ul class="menu-list">
+            <li><a href="#">システム開発</a></li>
+            <li><a class="is-active" href="#">技術教育</a>
+              <ul>
+                <li><a href="#">一社研修</a></li>
+                <li><a href="#">定期開催</a></li>
+              </ul>
+            </li>
+            <li><a href="#">ソフトウェア</a></li>
+          </ul>
+        </aside>
       </div>
 
       <div class="column">
-        本文
+        <article class="box media">
+          <figure class="media-left">
+            <p class="image is-64x64">x</p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p><strong>PIYO</strong></p>
+            </div>
+          </div>
+        </article>
+        <article class="box media">
+          <figure class="media-left">
+            <p class="image is-64x64">x</p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p><strong>BAR</strong></p>
+            </div>
+          </div>
+        </article>
       </div>
     </main>
+
+    <footer class="footer">
+       <div class="container">
+         <div class="content has-text-centered">
+           FUGA
+         </div>
+       </div>
+    </footer>
 
   </div>
 </template>
@@ -68,3 +115,9 @@
 export default {
 }
 </script>
+
+<style>
+main {
+  background-color: #eee;
+}
+</style>
